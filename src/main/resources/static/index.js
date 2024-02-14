281e58b45754d1301c48696b2c6a83f616b2d3ff
Aryokh04 <143172110+Aryokh04@users.noreply.github.com>
2024-02-14 23:59:36 +0100

    Created the function OrderTickets(). Defiend variables by getting the input value from index.html. Also made a persons object with the different inputs and pushed the object into the array.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,26 @@
 // Defining an empty array.
 let orderArray = [];
+
+// Making a function
+function orderTickets() {
+    // The function starts with creating variables and getting the value from the html
+    let chosenMovie = document.getElementById("selectmovie").value;
+    let amountOfTickets = document.getElementById("amount").value;
+    let theFirstName = document.getElementById("firstname").value;
+    let theLastName = document.getElementById("lastname").value;
+    let thePhoneNr = document.getElementById("phonenr").value;
+    let theEmail = document.getElementById("email").value;
+
+    // Creating an object "persons" with the variables.
+    let persons = {
+        chosenMovie,
+        amountOfTickets,
+        theFirstName,
+        theLastName,
+        thePhoneNr,
+        theEmail
+    };
+
+    // Pushing the object into the empty array.
+    orderArray.push(persons);
+}
