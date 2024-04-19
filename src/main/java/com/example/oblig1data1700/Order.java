f34b2737ec5d9acca5027d6f876dc0392b9d12f8
Aryokh04 <143172110+Aryokh04@users.noreply.github.com>
2024-04-19 15:58:54 +0200

    - remove lombok, plain pojo structure now

--- src/main/java/com/example/oblig1data1700/Order.java
+++ src/main/java/com/example/oblig1data1700/Order.java
@@ -1,14 +1,8 @@
 package com.example.oblig1data1700;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 // Defining each attribute for the Order object in java.
 // Using lombok dependency for creating a constructor, empty constructor and get/set.
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
+
 public class Order {
     private String chosenMovie;
     private String amount;
@@ -17,6 +11,67 @@ public class Order {
     private String phoneNr;
     private String email;
 
+    public Order(String chosenMovie, String amount, String firstName, String lastName, String phoneNr, String email) {
+        this.chosenMovie = chosenMovie;
+        this.amount = amount;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNr = phoneNr;
+        this.email = email;
+    }
+
+    public Order(){}
+
+    public String getChosenMovie() {
+        return chosenMovie;
+    }
+
+    public void setChosenMovie(String chosenMovie) {
+        this.chosenMovie = chosenMovie;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNr() {
+        return phoneNr;
+    }
+
+    public void setPhoneNr(String phoneNr) {
+        this.phoneNr = phoneNr;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
+
+
 
 
